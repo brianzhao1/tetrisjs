56b51e43d6e5e67ba34c00e2517f41bc6388dffc
hydrogen <hydrogen@quantum.home>
2020-05-22 23:47:55 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var boardWidth = 10;
+var boardHeight = 24;
+var unitSize = 25;
+
+function setup() {
+    createCanvas(boardWidth * unitSize, boardHeight * unitSize);
+}
+
+function draw() {
+    background(0);
+}
